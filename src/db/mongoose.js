7f9eb8068f22ed5cad758939872f8d81b871562d
soroushysfi <soroushysfi@gmail.com>
2021-04-06 23:43:40 -0700

    mongoose config updated

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -2,8 +2,7 @@ const mongoose = require('mongoose')
 
 mongoose.connect(process.env.MONGODB_URL, {
     useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true 
+    useCreateIndex: true
 })
 
 
