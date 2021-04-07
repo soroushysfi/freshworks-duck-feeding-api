a37d3ed7b080331795632ea0328ddc9c66904990
soroushysfi <soroushysfi@gmail.com>
2021-04-06 23:25:18 -0700

    added more test cases for the api

--- tests/duck-feeding-test.js
+++ tests/duck-feeding-test.js
@@ -1,15 +1,42 @@
 const request = require('supertest')
 const app = require('../src/app')
+const DuckFeeding = require('../src/models/duck-feeding-model')
+
+const duckFeedingInfo = {
+    feedingTime: "2018-12-31",
+    foodType: "test",
+    duckCount: 10,
+    foodAmount: 5,
+    reOccurring: true 
+}
+
+beforeEach(async () => {
+    await DuckFeeding.deleteMany({foodType: "test"})
+    await new DuckFeeding(duckFeedingInfo).save()
+})
+
 
 test('Should get duck feeding information', async () => {
     await request(app)
-    .get('/api/duck-feeding')
-    .send({
-        feedingTime: "2018-12-31",
-        foodType: "this is food type",
-        duckCount: 10,
-        foodAmount: 5,
-        reOccurring: true 
-    })
+    .get('/api/duck-feeding?limit=1')
     .expect(200)
+})
+
+test('Should create duck feeding information', async () => {
+    await request(app)
+    .post('/api/duck-feeding')
+    .send(duckFeedingInfo)
+    .expect(201)
+
+    const result =  await DuckFeeding.find({foodType: "test"})
+    expect(result.length).toBe(2)
+})
+
+test('Should not create duck feeding information', async () => {
+    await request(app)
+    .post('/api/duck-feeding')
+    .send({
+        foodType: "test"
+    })
+    .expect(500)
 })
