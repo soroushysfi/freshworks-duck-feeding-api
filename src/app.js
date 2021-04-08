6a6fa5ee55e3b01487fb49825436cd1a58fe1500
soroushysfi <soroushysfi@gmail.com>
2021-04-07 20:42:05 -0700

    add cors

--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
 const express = require('express')
+const cors = require('cors')
 require('./db/mongoose')
 const DuckFeedingRouter = require('./routes/duck-feeding-router')
 
 
 const app = express()
 
-
+app.use(cors());
 app.use(express.json());
 
 app.use('/api', DuckFeedingRouter);
